e416ec3bcc3d7e1082885b0e72d204832166b91c
bogdan-curta <120948663+bogdan-curta@users.noreply.github.com>
2022-12-18 16:33:30 +0000

    fixes

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ module "blog_alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_isntance.blog.id
+          target_id = aws_instance.blog.id
           port = 80
         }
       }
@@ -85,7 +85,7 @@ module "blog_sg" {
   version = "4.16.2"
   name = "blog"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
